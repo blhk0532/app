8fef48e78021f41797e0fca388da6cfd403d7c57
blhk0532 <bjorn.leon.henry.kristoffersson@gmail.com>
2026-03-25 16:37:06 +0700

    AboutEditorWidget

--- plugins/cachethq/core/src/Filament/Widgets/AboutEditorWidget.php
+++ plugins/cachethq/core/src/Filament/Widgets/AboutEditorWidget.php
@@ -13,7 +13,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 
-class StatusAboutWidget extends Widget
+class AboutEditorWidget extends Widget
 {
     protected static bool $isLazy = false;
 
